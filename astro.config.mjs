014a6cea9e751cf09362b3b7aef87f9a3afa3b94
Matt Willis <mewillis21@gmail.com>
2025-01-31 19:47:54 -0500

    GA and Step list formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push']
+      }
+    })
+  ]
 });
