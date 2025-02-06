3a358689f40eff75857ec4ea663d25e09e275693
Matt Willis <mewillis21@gmail.com>
2025-02-05 20:22:59 -0500

    remove partytown - replace with googleanalytics plugin / add back collapse in actions page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,11 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), mdx(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), sitemap(), tailwind()]
+  integrations: [icon(), mdx(), sitemap(), tailwind()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/preline/preline.js'
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
 	],
 	theme: {
 		extend: {},
 	},
-	plugins: [
-		require('preline/plugin')
-	],
 }
