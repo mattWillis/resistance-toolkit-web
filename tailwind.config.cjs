abe75202e8eea713adde3029ac84f21578fd3b1e
Matt Willis <mewillis21@gmail.com>
2025-02-17 18:44:43 -0500

    styling for causes homepage / remove Causes nav button / Sticky Actions menu / styling tweaks to actions page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/preline/preline.js'
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		require('@tailwindcss/typography')
+		require('@tailwindcss/typography'),
+		require('preline/plugin'),
 	],
 }
